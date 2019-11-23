e648baf908429921a0b5175d4c0acb76db8d5282
MartaKaminska <j.martakaminska@gmial.com>
2019-11-23 20:44:19 +0100

    projekt.v1

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'nested'
+        }))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('default', gulp.series('sass'), function () {
+});
+
+gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+});
