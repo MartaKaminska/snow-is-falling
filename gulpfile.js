1b182b4d81486e1cf41c112179c4707f3709f891
MartaKaminska <j.martakaminska@gmial.com>
2019-11-23 21:29:52 +0100

    Update 2019-11-23T20:29:43.893Z

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'nested'
+        }))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('default', gulp.series('sass'), function () {
+});
+
+gulp.task('deploy', function() {
+    return gulp.src('./**/*')
+    .pipe(ghPages());
+})
+
+gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+});
